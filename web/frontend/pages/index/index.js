419b690790fc4313a2467d8d7a9d5dc3e737ee4b
Dinesh Umasankar <dqu5021@psu.edu>
2023-04-01 17:42:19 +0000

    Camera Feed into Video Element

--- web/frontend/pages/index/index.js
+++ web/frontend/pages/index/index.js
@@ -1,25 +1,32 @@
 let mediaStream = null;
+let video = document.getElementById("liveCam");
 
 let constraints = {
-    video: true,
+    video: {
+        width: 1200,
+        height: 600,
+    },
     audio: false
 }
 
-// const getMediaStream = async (constraints) => {
-//     try {
-//         mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
-//         let video = document.getElementById("liveCam")
-//         video.srcObject = mediaStream;
-//         video.onloadedmetadata = function(e) {
-//             video.play();
-//         }
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
+const getMediaStream = (constraints) => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+            
+            video.srcObject = stream;
+            video.onloadedmetadata = function(e) {
+                video.play();
+            }
+        }).catch(function(err) {
+            console.log(err)
+        });
+    } else {
+        alert('Your browser does not support getUserMedia API')
+    }
+}
 
 const startCameraButton = document.getElementById("startCamera").addEventListener("click", () => {
-    console.log('You clicked me!')
+    getMediaStream(constraints);
 });
 
 const snapPictureButton  = document.getElementById("snapPicture").addEventListener("click", () => {
